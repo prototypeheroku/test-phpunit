7bd9fcd96328224c7ea741c290a004c55190f6f2
caoyangit <caoyan0728@gmail.com>
2017-12-19 17:06:16 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,4 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('foo', function () {
-    return 'Foo!';
-});
-
-Route::get('foo/foo1', 'FooController@foo1');
-
 Route::get('foo/foo2', 'FooController@foo2');
