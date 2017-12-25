1f57686557f7490ba957ab4182d200f6f58017de
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2017-12-25 18:13:18 +0900

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,13 +1,13 @@
 <form action="{{ url('posts/store') }}" method="post">
     {{ csrf_field() }}
-    {{ method_field('GET') }}
+    {{ method_field('POST') }}
     <div>
         <label for="title">{{ __('Title') }}</label>
         <input id="title" type="text" name="title" required autofocus>
     </div>
     <div>
         <label for="comment">{{ __('Comment') }}</label>
-        <textarea id="comment" name="comment" rows="8" required></textarea>
+        <input id="comment" type="text" name="comment" required autofocus>
     </div>
     <button type="submit" name="submit">{{ __('Submit') }}</button>
 </form>
