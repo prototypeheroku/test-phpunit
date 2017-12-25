6aa70c99d8effdc097ded72faf455b228969f740
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2017-12-25 18:24:20 +0900

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,11 +1,11 @@
 <form action="{{ url('posts/add') }}" method="post">
     <div>
         <label for="title">{{ __('Title') }}</label>
-        <input id="title" type="text" name="title" required autofocus>
+        <input id="title" type="text" name="title" value="{{ $post->title }}" required autofocus>
     </div>
     <div>
         <label for="comment">{{ __('Comment') }}</label>
-        <input id="comment" type="text" name="comment" required autofocus>
+        <input id="comment" type="text" name="comment" value="{{ $post->comment }}" required autofocus>
     </div>
     <button type="submit" name="submit">{{ __('Submit') }}</button>
 </form>
