9e5e328500b38f893afbdcbee3fd569aaf3bb1fa
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2017-12-25 16:44:01 +0900

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,12 +3,14 @@
         <thead>
             <tr>
                 <th>{{ __('Title') }}</th>
+                <th>{{ __('備考') }}</th>
             </tr>
         </thead>
         <tbody>
         @foreach ($posts as $post)
             <tr>
                 <td>{{ $post->title }}</td>
+                <td>{{ $post->comment }}</td>
              </tr>
         @endforeach
         </tbody>
