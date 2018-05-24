b3737b4ea32e36d93b3cf752513b8392b30b080d
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-05-25 03:15:25 +0900

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,8 +11,9 @@
     </div>
     
     <!--↓↓ 検索フォーム ↓↓-->
-    <div class="col-sm-4" style="padding:20px 0; padding-left:0px;">
-    <form class="form-inline" action="{{url('posts')}}" method="post">
+    <div class="col-sm-4">
+    #<form class="form-inline" action="{{url('posts')}}" method="post">
+    <form class="form-inline">
       <div class="form-group">
       <input type="text" name="keyword" value="{{$keyword}}" class="form-control" placeholder="コードを入力してください">
       </div>
