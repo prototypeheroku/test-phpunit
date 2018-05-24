b17cbbec625ccb4c861d2f0d522e29cf3e80f38e
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-05-25 02:13:13 +0900

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,6 +9,17 @@
             <a href="/posts/create" class="btn btn-primary" style="margin:20px;">新規登録</a>
         </div>
     </div>
+    
+    <!--↓↓ 検索フォーム ↓↓-->
+    <div class="col-sm-4" style="padding:20px 0; padding-left:0px;">
+    <form class="form-inline" action="{{url('/crud')}}">
+      <div class="form-group">
+      <input type="text" name="keyword" value="{{$keyword}}" class="form-control" placeholder="名前を入力してください">
+      </div>
+    <input type="submit" value="検索" class="btn btn-info">
+    </form>
+    </div>
+    <!--↑↑ 検索フォーム ↑↑-->
 
     <!-- table -->
     <table class="table table-striped">
