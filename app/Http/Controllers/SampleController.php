2ce5459953a5d3ca9b167ec29609ef94ad895f8c
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-05-28 00:15:53 +0900

    Update SampleController.php

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -20,9 +20,9 @@ class SampleController extends Controller
     public function index(Request $request)
     {
 	//検索結果を取得
-	//$query = DB::select('select colkey1,col1,col2 from sample where colkey1 = ?', [$request->colkey1]);
+	$query = DB::select('select colkey1,col1,col2 from sample where colkey1 = ?', [$request->colkey1]);
 	//$query = DB::select('select * from sample ');
-	$query = Sample::query();
+	//$query = Sample::query();
 	    
         //ページネーション
         $samples = $query->orderBy('colkey1','asc')->paginate(5);
