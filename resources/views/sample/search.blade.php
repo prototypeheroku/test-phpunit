703f04ad707a31d90534d385ada6583c00ef0747
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-05-27 19:03:54 +0900

    Update search.blade.php

--- resources/views/sample/search.blade.php
+++ resources/views/sample/search.blade.php
@@ -8,15 +8,13 @@
     
         <div class="form-group">
             <label>コード</label>
-            <input type="text" name="title" value="{{ $post->title }}" class="form-control" required autofocus>
+            <input type="text" name="title" value="" class="form-control" required autofocus>
         </div>
 
         <div class="form-group">
             <label>内容</label>
-            <input type="text" name="comment" value="{{ $post->comment }}" class="form-control" required autofocus>
+            <input type="text" name="comment" value="" class="form-control" required autofocus>
         </div>
-
-        <input type="hidden" name="_token" value="{{csrf_token()}}">
 
         <input type="submit" value="検索" class="btn btn-primary">
 　　</form>
