363af9a2b633db6cad566c9bce873df51bb414fa
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-05-27 22:02:17 +0900

    Update index.blade.php

--- resources/views/sample/index.blade.php
+++ resources/views/sample/index.blade.php
@@ -3,8 +3,11 @@
 @section('content')
 
     <h1>一覧表示</h1>
-
+    
     <div class="row">
+        <div class="col-sm-12">
+            <a href="/sample" class="btn btn-primary" style="margin:20px;">一覧に戻る</a>
+        </div>
         <div class="col-sm-12">
             <a href="/sample/create" class="btn btn-primary" style="margin:20px;">新規登録</a>
         </div>
