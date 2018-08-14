6d8672c5aa921218bb2e3e94047d3ee1c179d04b
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-08-14 23:51:08 +0900

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -34,7 +34,7 @@
 						<label for="disastername" class="control-label">災害名（必須）</label>
 					</div>
 					<div class="col-xs-4">
-						<input type="text" name="title" value="{{ $post->title }}" class="form-control" placeholder=""　required autofocus>
+						<input type="text" name="disaster_name" value="{{ $post->disaster_name }}" class="form-control" placeholder=""　required autofocus>
 					</div>
 					<div class="col-xs-4"></div>
 				</div>
@@ -46,7 +46,19 @@
 						<label for="disasterheadname" class="control-label">災害種別</label>
 					</div>
 					<div class="col-xs-4">
-						<input type="text" name="comment" value="{{ $post->comment }}" class="form-control" placeholder=""　required autofocus>
+						<input type="text" name="disaster_kind" value="{{ $post->disaster_kind }}" class="form-control" placeholder=""　required autofocus>
+					</div>
+					<div class="col-xs-3"></div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-xs-2"></div>
+					<div class="col-xs-2 text-left">
+						<label for="disasterheadarea" class="control-label">災害地域</label>
+					</div>
+					<div class="col-xs-4">
+						<input type="text" name="disaster_area" value="{{ $post->disaster_area }}" class="form-control" placeholder=""　required autofocus>
 					</div>
 					<div class="col-xs-3"></div>
 				</div>
