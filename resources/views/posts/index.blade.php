22d193aceff9879d14c04e09e29b353832bda18c
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-08-15 02:10:23 +0900

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -71,6 +71,7 @@
                 <td style="width:15%">{{ $post->disaster_area }}</td>
                 <td>
 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　<a href="/posts/show/{{$post->disaster_name}}" class="btn btn-primary btn-sm">詳細</a>
+                </td>
                 <td>
 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　<a href="/posts/edit/{{$post->disaster_name}}" class="btn btn-primary btn-sm">編集</a>
                 </td>
